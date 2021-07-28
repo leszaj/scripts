a33ded72fedfba21e4836db6c1f5c0ddfb3170c7
Bartosz Leszaj <73910351+leszaj@users.noreply.github.com>
2021-07-28 08:14:42 +0200

    Update func_args_via_star.py

--- python/codility/func_args_via_star.py
+++ python/codility/func_args_via_star.py
@@ -8,3 +8,11 @@ def rozpakowywanie(pierwszy, drugi, trzeci):
 
 lista_argumentow = [1,3,5] 
 rozpakowywanie(*lista_argumentow) 
+
+
+"""
+
+    # same as:
+    rozpakowywanie(lista_argumentow[0], lista_argumentow[1], lista_argumentow[2])
+    
+"""
